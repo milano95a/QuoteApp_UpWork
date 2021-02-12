c63cfa0edd0a80e4239ca2f80a52f45504d1bcbc
abu <milano95j@gmail.com>
2021-02-12 10:57:32 +0500

    load quotes

--- QuoteApp_UpWork/HomeViewController.swift
+++ QuoteApp_UpWork/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 private let reuseIdentifier = "MyCell"
 private let STATUS_BAR_HEIGHT = 20
@@ -14,6 +15,8 @@ class HomeViewController: UIViewController,
                           UICollectionViewDelegate,
                           UICollectionViewDataSource,
                           UICollectionViewDelegateFlowLayout{
+    
+    let db = Database.database().reference()
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -26,28 +29,53 @@ class HomeViewController: UIViewController,
         return layout
     }()
     
-    
-    let array = [
-        "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
-        "Be yourself; everyone else is already taken.",
-        "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-        "So many books, so little time.",
-        "A room without books is like a body without a soul.",
-        "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
-        "You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.",
-        "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
-        "You only live once, but if you do it right, once is enough.",
-        "Be the change that you wish to see in the world.",
-        "In three words I can sum up everything I've learned about life: it goes on.",
-        "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
-        "Don’t walk in front of me… I may not follow Don’t walk behind me… I may not lead Walk beside me… just be my friend",
-        "No one can make you feel inferior without your consent.",
-    ]
+//    var array = [
+//        "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+//        "Be yourself; everyone else is already taken.",
+//        "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+//        "So many books, so little time.",
+//        "A room without books is like a body without a soul.",
+//        "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+//        "You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.",
+//        "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+//        "You only live once, but if you do it right, once is enough.",
+//        "Be the change that you wish to see in the world.",
+//        "In three words I can sum up everything I've learned about life: it goes on.",
+//        "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+//        "Don’t walk in front of me… I may not follow Don’t walk behind me… I may not lead Walk beside me… just be my friend",
+//        "No one can make you feel inferior without your consent.",
+//    ]
+    var array = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.collectionViewLayout = layout
+        
+        var newArray = [String]()
+        
+        db.child("quotes").observeSingleEvent(of: .value) { [weak self] snapshot in
+            guard let quotes = snapshot.value as? [String: Any] else {
+                return
+            }
+            for quote in quotes.values {
+                guard let quoteText = quote as? [String: String] else {
+                    return
+                }
+                newArray.append(quoteText.values.first!)
+            }
+            
+            DispatchQueue.main.async {
+                self?.array = newArray
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewWillAppear")
     }
     
     // MARK: UICollectionViewDataSource
